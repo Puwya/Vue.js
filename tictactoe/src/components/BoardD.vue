2ae46d228542146b4431220de748a00992d9005f
Carlos Aguilera <18aguilerac@gmail.com>
2022-03-05 22:03:58 -0800

    adding

---
+++ tictactoe/src/components/BoardD.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Board</h1>
+    <div v-for="i in 3" :key="i">
+        <button v-for="j in 3" :key="j">
+          {{i}}, {{j}}
+        </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log(this.i,this.j,this.board);
+  },
+  data() {
+    return {
+      board: [
+          ['','',''],
+          ['','',''],
+          ['','','']
+      ]
+    }
+  }
+};
+</script>
+
+<style>
+</style>
