9a25e4cf46709e42ddecb3c56c69b82d7dd525d5
Carlos Aguilera <18aguilerac@gmail.com>
2022-08-14 14:29:57 -0700

    added bootstrap (always takes me ten years

--- MEVN-Posting/mevn-post/src/components/Post.vue
+++ MEVN-Posting/mevn-post/src/components/Post.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <p v-for="post in posts" :key="post._id">
-      {{ post.post_name }}
+      {{ post.name }}
     </p>
+    <button class="btn btn-primary"></button>
   </div>
 </template>
 
@@ -27,21 +28,3 @@ export default {
   },
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
