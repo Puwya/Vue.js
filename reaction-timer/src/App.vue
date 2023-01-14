d6c6ab79d6a4e472701864323f1c80b09a36186b
Carlos Aguilera <18aguilerac@gmail.com>
2023-01-14 14:45:03 -0800

    ommmmmgg

---
+++ reaction-timer/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <h1>Ninja Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <SmartBlock v-if="isPlaying" :delay="delay" />
+</template>
+
+<script>
+import SmartBlock from '@/components/smart-block.vue';
+
+export default {
+  components: {
+    SmartBlock,
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ reaction-timer/src/components/smart-block.vue
@@ -0,0 +1,36 @@
+<template>
+  <div v-if="showBlock" class="block">click me</div>
+</template>
+
+<script>
+export default {
+  props: {
+    delay: {
+      default: 2000,
+    },
+  },
+  data() {
+    return {
+      showBlock: false,
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showBlock = true;
+    }, this.delay);
+  },
+};
+</script>
+
+<style scoped>
+.block {
+  width: 500px;
+  height: 250px;
+  background: moccasin;
+  margin: 40px auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 5px;
+}
+</style>
