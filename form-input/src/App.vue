9b1e04c8bd92336891e62ff75b7dc3ac00fc258a
Carlos Aguilera <18aguilerac@gmail.com>
2023-01-14 17:43:35 -0800

    form input init

---
+++ form-input/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <smart-form />
+</template>
+
+<script>
+import smartForm from '@/components/smart-form.vue';
+
+export default {
+  name: 'App',
+  components: {
+    smartForm,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+body {
+  margin: 0;
+  background-color: #eee;
+}
+</style>
